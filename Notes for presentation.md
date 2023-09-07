a37d81e1a1e053489cf8026996286622d43ff7ae
duong nguyen <melody.duong.nguyen@gmail.com>
2023-09-07 11:57:19 +0900

    Remove notebook folder

--- Notes for presentation.md
+++ Notes for presentation.md
@@ -6,3 +6,25 @@ metion in preprocessing step in data part
 explain the alarm part (maybe use better alarm sound or design the warning part on screen)
 add the alarm 30 frames detected as drowsy => add to streamlit
 new video around 20 seconds, showing clearly alert and drowsy
+
+
+=======================
+Fri morning: no teacher before 10
+part time students will come from 2
+Demo Day at 6
+order will be sent soon
+mixed btw all groups
+no Q&A
+everything needs to be set up before demo day
+need streamlit cloud
+
+=======================
+team slide will be at the end
+data part: the slide of 6pics does not match with the speech
+should turn it into a slide of video and hundreds of pics and add the slide after the slide of 2 models
+introduce model and the way we use it in our process
+a lot of scrolling in streamlit,
+loger video
+add some important info on the slide
+diagram of the process in one slide, starting from data to the end result, can put in the beginning or at the end
+data augmenttation demonstration
--- app/app.py
+++ app/app.py
@@ -8,68 +8,96 @@ from PIL import Image
 import os
 import pygame
 from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-# Charger le modèle YOLOv5
+# from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import sys
+from drowsiness_detection.utils.params import *
+
+# Load the YOLOv5 model
 if 'model' not in st.session_state:
-    st.session_state['model'] = YOLO('/home/giovanni/code/GiovanniJolard/test_yolo_streamlit/runs_10/detect/train5/weights/best.pt')
+    st.session_state['model'] = YOLO(
+        os.path.join(PARENT_DIRECTORY,
+                     'runs',
+                     'BEST_runs_10th_tuned_with_pics_of_8',
+                     'detect',
+                     'train5',
+                     'weights',
+                     'best.pt'
+                     )
+    )
 model = st.session_state['model']
-# A installer ffprobe alert_sound.wav
-#211C3E
-#model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+# Install ffprobe alert_sound.wav
+# 211C3E
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 # model = YOLO('/home/giovanni/code/GiovanniJolard/test_yolo_streamlit/runs_6/detect/yolov8s/weights/best.pt')
 # Initialization
 # model = YOLO('/home/giovanni/code/GiovanniJolard/test_yolo_streamlit/runs_6/detect/yolov8s/weights/best.pt')
-# Configuration de la page Streamlit
+# Streamlit page configuration
 st.set_page_config(
     page_title="Siesta Sentry",
     page_icon=":male-detective:",
-    layout="wide",
+    # layout="wide",
     initial_sidebar_state="expanded"
 )
-# Charger l'image pour le titre
-title_image = Image.open("/mnt/c/Users/ruiz-/OneDrive/Images/SIESTA8LOGO.png")
-# Afficher l'image du titre
+
+# Load the image for the title
+title_image = Image.open(os.path.join(
+    PARENT_DIRECTORY,
+    'logo',
+    'SIESTA8LOGO.png'
+))
+
+# Display the title image
 st.image(title_image, use_column_width=True)
-# Charger le son d'alerte
-alert_sound_path = 'alert.wav'
-# Initialiser pygame mixer
+
+# Load the alert sound
+alert_sound_path = os.path.join(PARENT_DIRECTORY,
+                                'alarm_sound',
+                                'alarm.wav')
+
+# Initialize pygame mixer
 pygame.mixer.init()
-# Champ de saisie pour l'URL de la vidéo YouTube ou le chemin du fichier
+
+# Input field for YouTube video URL or video file path
 st.write("\n\n")
-video_input_option = st.radio("Choisissez une option :", ("URL de la vidéo YouTube", "Chemin du fichier vidéo"))
-if video_input_option == "URL de la vidéo YouTube":
-    video_url = st.text_input("Entrez l'URL de la vidéo YouTube :")
+video_input_option = st.radio("Choose an option:", ("YouTube Video URL", "Video File Path"))
+if video_input_option == "YouTube Video URL":
+    video_url = st.text_input("Enter the YouTube video URL:")
     video_path = False
 else:
     video_url = False
-    video_path = st.file_uploader("Téléchargez une vidéo", type=["mp4"])
-# Vérifier si l'utilisateur a soumis une URL de vidéo ou téléchargé une vidéo
-if st.button("Démarrer la Détection") and (video_url or video_path):
+    video_path = st.file_uploader("Upload a video", type=["mp4","mov"])
+
+# Check if the user submitted a video URL or uploaded a video
+if st.button("Start Detection") and (video_url or video_path):
     try:
         if video_url:
-            # Télécharger la vidéo YouTube
+            # Download the YouTube video
             yt = YouTube(video_url)
             video_stream = yt.streams.filter(file_extension="mp4").first()
             video_path = video_stream.download()
         elif video_path is not None:
-            # Enregistrer temporairement le fichier téléchargé
+            # Temporarily save the uploaded file
             with open(video_path.name, "wb") as f:
                 f.write(video_path.read())
             video_path = video_path.name
-        # Ouvrir la vidéo en streaming
+
+        # Open the video for streaming
         cap = cv2.VideoCapture(str(video_path))
         video_elem = st.empty()
         running = True
-        stop_button = st.button("Arrêter la Détection")
+        stop_button = st.button("Stop Detection")
         drowsy_counter = 0
         threshold_frames = 5
         last_alert_time = 0
+
         while running:
             ret, frame = cap.read()
             if not ret or stop_button:
                 running = False
                 break
-            # Effectuer la détection
+
+            # # Perform detection
             results = model(frame)
             for result in results:
                 names_dict = result.names
@@ -78,29 +106,42 @@ if st.button("Démarrer la Détection") and (video_url or video_path):
                     if drowsy_counter >= threshold_frames:
                         current_time = time.time()
                         if current_time - last_alert_time >= 20:
-                            # Jouer le son d'alerte
+                            # Play the alert sound
                             pygame.mixer.Sound(alert_sound_path).play()
                             last_alert_time = current_time
-                            st.info("Endormi détecté !")
+                            st.info("Drowsiness Detected!")
                 else:
                     drowsy_counter = 0
-                # Utilisez l'attribut result.orig_img pour afficher l'image originale
+
+                # Use the result.orig_img attribute to display the original image
                 detection_image = np.array(result.orig_img)
-                # Dessinez les boîtes de détection sur l'image (si result.boxes contient les coordonnées)
+
+                # Draw detection boxes on the image (if result.boxes contains coordinates)
                 if result.boxes is not None:
                     for box, conf, cls in zip(result.boxes.xyxy, result.boxes.conf, result.boxes.cls):
                         x1, y1, x2, y2 = box.tolist()
                         class_name = names_dict[int(cls)]
-                        label = f"{class_name} ({conf:.2f})"
-                        # Dessiner la boîte de détection et le label
-                        cv2.rectangle(detection_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                        cv2.putText(detection_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                # Afficher l'image de détection
+                        if class_name == "alert":
+                            label = f"{class_name} ({conf:.2f})"
+                            # Draw detection box and label
+                            cv2.rectangle(detection_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+                            cv2.putText(detection_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        elif class_name == "drowsy":
+                            label = f"{class_name} ({conf:.2f})"
+                            # Draw detection box and label
+                            cv2.rectangle(detection_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255, 2))
+                            cv2.putText(detection_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+                # Display the detection image
                 video_elem.image(detection_image, channels="BGR", use_column_width=True)
                 cv2.waitKey(1)
-        # Afficher un message lorsque la détection est arrêtée
-        st.write("Détection arrêtée")
+                # st_video_with_fixed_width(detection_image, target_width)
+                # cv2.waitKey(1)
+
+        # Display a message when detection is stopped
+        st.write("Detection stopped")
+
     except Exception as e:
-        # Afficher un message d'erreur en cas de problème
-        st.error("Une erreur s'est produite lors du traitement de la vidéo.")
+        # Display an error message if there's a problem
+        st.error("An error occurred while processing the video.")
         st.error(str(e))
